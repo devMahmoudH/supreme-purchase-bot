097a4feeef6453d2e4bfb74fea59579e97d1a65d
Mahmoud <mahmoudhadbeh@gmail.com>
2020-03-03 21:28:25 -0500

    Added all files

---
+++ config.py
@@ -0,0 +1,14 @@
+#Here you enter the info you want to be autfilled.
+
+
+
+keys = {
+		"product_url": "https://www.supremenewyork.com/shop/skate/vms6tr8nz", #Enter Product URL
+		"name": "First and Last Name", #Enter First and Last Name
+		"email": "my@email.com", #Enter Email
+		"address": "103 Ellis Way", #Enter Street Address
+		"zip": 31211, #Enter Zip Code
+		"phone_number": 8625015781, #Enter your phone number
+		"card_number": 5333619503715702, #Enter Card Number 
+		"cvv": 321 #Enter CVV
+} 
